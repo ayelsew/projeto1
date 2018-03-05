e517f260536a6eda2c85e8176c4d45ddaf1c3a5a
wesley <wesley.uk0110@gmail.com>
2018-03-05 17:28:55 -0300

    js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,14 @@
-document.addEventListener('DOMContentLoaded',function () {
-   
+document.addEventListener("DOMContentLoaded", function() {
+  let navbar = document.querySelector("nav");
+  let header = document.querySelector("header");
+  window.onscroll = function() {
+    if (window.scrollY >= header.offsetHeight) {
+      navbar.classList.remove("navincolor");
+      console.log("ativo");
+    } else {
+      navbar.classList.add("navincolor");
+      console.log("desativado");
+    }
+    /* console.log(window.scrollY + " - " + header.offsetHeight); */
+  };
 });
